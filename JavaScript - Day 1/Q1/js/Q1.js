a2db53801e5c56078c806179e8e432263e5afb4f
Raviv-23795 <vermaravi23795@gmail.com>
2020-05-11 19:47:57 +0530

    updated Q1

--- JavaScript - Day 1/Q1/js/Q1.js
+++ JavaScript - Day 1/Q1/js/Q1.js
@@ -1,10 +1,10 @@
-function highestnum()
+function highestnum(num1,num2,num3)
 {
-	var num1,num2,num3;
+	/*var num1,num2,num3;
 	num1 = Number(document.getElementById("n1").value);
 	num2 = Number(document.getElementById("n2").value);
 	num3 = Number(document.getElementById("n3").value);
-
+*/
 	if(num1>num2 && num1>num3)
 	{
 		document.getElementById("high").innerHTML = "The highest number is "+num1+"<br/><br/>";
